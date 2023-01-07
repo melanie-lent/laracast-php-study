04267ec53bfb72baeb83cf03a041b99cb4b4ab51
d00188878 <79728010+melanie-lent@users.noreply.github.com>
2023-01-07 22:15:27 +0900

    made a list using a dictionary

--- hello.php
+++ hello.php
@@ -11,12 +11,20 @@
         </meta>
     </head>
     <body>
-        <?php
-            $fruit = ["apple", "pear", "peach", "grape"];
-        ?>
         <header>
             <h1>aaaaaaa<?php echo "hi"; ?></h1>
         </header>
+        <h3>simple array stuff</h3>
+        <?php
+            $fruit = ["apple", "pear", "peach", "grape"];
+            $cat = [
+                'fur color' => 'black',
+                'name' => 'lucy',
+                'morals' => null,
+                'collar' => false ,
+                'smartness' => 0.05
+            ];
+        ?>
         <ul>
             <?php foreach ($fruit as $f) : ?>
                 <li><?= $f; ?></li>
@@ -26,6 +34,12 @@
                     echo "<li>$f</li>";
                 }
             ?>
+        </ul>
+        <h3>associative array stuff</h3>
+        <ul>
+            <?php foreach ($cat as $key => $val) : ?>
+                <li><?php echo "$key: $val"; ?></li>
+            <?php endforeach ?>
         </ul>
     </body>
 </html>
